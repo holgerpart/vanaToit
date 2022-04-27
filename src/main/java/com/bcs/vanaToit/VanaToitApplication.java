57b27c56108c4469ca65467c382081687cc0c8e9
holgerpart <Hobevalge1>
2022-04-27 14:04:06 +0300

    Initial commit

---
+++ src/main/java/com/bcs/vanaToit/VanaToitApplication.java
@@ -0,0 +1,13 @@
+package com.bcs.vanaToit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanaToitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanaToitApplication.class, args);
+	}
+
+}
