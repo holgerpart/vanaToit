22d7299fb4a72802dd6115693dacaab63c723694
holgerpart <Hobevalge1>
2022-04-28 16:44:17 +0300

    MASTER merge HOLGER to MASTER

---
+++ src/main/java/com/bcs/vanaToit/service/food/FoodService.java
@@ -0,0 +1,12 @@
+package com.bcs.vanaToit.service.food;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoodService {
+
+
+    public void addArticle(ArticleRequest articleRequest) {
+
+    }
+}
--- src/main/java/com/bcs/vanaToit/shop/ProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bcs.vanaToit.shop;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-}
