bd50a1b6ba6b84f3432456363aec14ccef4047fe
holgerpart <Hobevalge1>
2022-04-29 10:00:50 +0300

    update

--- src/main/java/com/bcs/vanaToit/domain/food/food/ArticleService.java
+++ src/main/java/com/bcs/vanaToit/domain/food/food/ArticleService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service
-public class FoodService {
+public class ArticleService {
 
     @Resource
     private FoodRepository foodRepository;
